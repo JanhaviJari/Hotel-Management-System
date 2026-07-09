fffc557d4a2fb8ddcc12d70a80d6c41e21896444
Janhavi Jari <147237630+JanhaviJari@users.noreply.github.com>
2026-07-09 21:09:47 +0530

    Remove database password from DBConnection

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "JJ15@jjjj";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
 
